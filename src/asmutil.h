511fd883babef4c685bf048ca372810ec2fb0c36
Captain-Calc <57052406+Captain-Calc@users.noreply.github.com>
2021-07-07 18:32:34 -0700

    Remove CopyVRAM code

--- src/asmutil.h
+++ src/asmutil.h
@@ -3,8 +3,6 @@
 
 #include <stdint.h>
 
-void asm_CopyVRAM(uint24_t x_src, uint8_t y_src, uint24_t width, uint8_t height, uint24_t x_dest, uint8_t y_dest);
-
 int8_t asm_GetCSC(void);
 void asm_CopyData(void *from, void *to, uint24_t amount, uint8_t copy_direction);
 uint8_t asm_LowToHighNibble(uint8_t nibble);
