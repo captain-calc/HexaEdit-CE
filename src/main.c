a2f49c59e486792d3b19bcd94e19819a8f24c141
Captain-Calc <57052406+Captain-Calc@users.noreply.github.com>
2021-07-12 19:16:58 -0700

    main.c formatting changes

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
+// Name:        Captain Calc
+// Date:        July 12, 2021
+// File:        main.c
+// Description: Loads default color theme, checks for Headless Start, and runs
+//              main menu.
 
-/*-----------------------------------------
- * Program Name: HexaEdit CE
- * Version:      See menu.h
- * Author:       Captain Calc   
- * Description:  A hex editor for the TI-84
- *               Plus CE.
- *-----------------------------------------
-*/
+// PROGRAM INFO
+//
+// Version:     See PROGRAM_VERSION in menu.h
+// Description: A hex editor for the TI-84 Plus CE.
+
 
 #include "colors.h"
 #include "editor.h"
@@ -22,61 +24,15 @@
 
 #include <debug.h>
 
+
+// Global structure for color theme configuration
 color_theme_config_t color_theme;
 
 
-static void load_default_color_theme(void)
-{
-	color_theme.background_color = LT_GRAY;
-	color_theme.bar_color = DK_GRAY;
-	color_theme.bar_text_color = WHITE;
-	color_theme.table_bg_color = WHITE;
-	color_theme.table_text_color = BLACK;
-	color_theme.selected_table_text_color = WHITE;
-	color_theme.cursor_color = BLUE;
-	return;
-}
+// Function Prototypes
+static void load_default_color_theme(void);
+static bool headless_start(void);
 
-/*
-static bool headless_start(void)
-{
-	ti_var_t config_data_slot;
-	
-	if ((config_data_slot = ti_Open(HS_CONFIG_APPVAR, "r")))
-	{
-		editor_HeadlessStart();
-    ti_Close(config_data_slot);
-		return true;
-	};
-  
-	return false;
-}
-*/
-
-static bool headless_start(void)
-{
-	ti_var_t slot;
-  char hs_config_ans_flag[HS_CONFIG_ANS_FLAG_LEN];
-  bool start_headless = false;
-	
-  ti_CloseAll();
-  
-	if ((slot = ti_OpenVar(ti_Ans, "r", TI_STRING_TYPE)))
-	{
-    ti_Read(&hs_config_ans_flag, HS_CONFIG_ANS_FLAG_LEN, 1, slot);
-    
-    if (!strncmp(hs_config_ans_flag, HS_CONFIG_ANS_FLAG, HS_CONFIG_ANS_FLAG_LEN))
-    {
-dbg_sprintf(dbgout, "hs_config_ans_flag = %s\n", hs_config_ans_flag);
-      editor_HeadlessStart();
-      start_headless = true;
-    };
-
-    ti_Close(slot);
-	};
-  
-	return start_headless;
-}
 
 int main(void)
 {
@@ -101,4 +57,43 @@ int main(void)
 	
 	gfx_End();
 	return 0;
+}
+
+
+static void load_default_color_theme(void)
+{
+	color_theme.background_color = LT_GRAY;
+	color_theme.bar_color = DK_GRAY;
+	color_theme.bar_text_color = WHITE;
+	color_theme.table_bg_color = WHITE;
+	color_theme.table_text_color = BLACK;
+	color_theme.selected_table_text_color = WHITE;
+	color_theme.cursor_color = BLUE;
+	return;
+}
+
+
+static bool headless_start(void)
+{
+	ti_var_t slot;
+  char hs_config_ans_flag[HS_CONFIG_ANS_FLAG_LEN];
+  bool start_headless = false;
+	
+  ti_CloseAll();
+  
+	if ((slot = ti_OpenVar(ti_Ans, "r", TI_STRING_TYPE)))
+	{
+    ti_Read(&hs_config_ans_flag, HS_CONFIG_ANS_FLAG_LEN, 1, slot);
+    
+    if (!strncmp(hs_config_ans_flag, HS_CONFIG_ANS_FLAG, HS_CONFIG_ANS_FLAG_LEN))
+    {
+dbg_sprintf(dbgout, "hs_config_ans_flag = %s\n", hs_config_ans_flag);
+      editor_HeadlessStart();
+      start_headless = true;
+    };
+
+    ti_Close(slot);
+	};
+  
+	return start_headless;
 }
